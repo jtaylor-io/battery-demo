2018f3f401e2249e24d6c5a83a22bc44df40cf14
John Taylor <johnmichaeltaylor79@gmail.com>
2024-02-07 13:36:39 +0000

    Next product feature on home page

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import APIRouter, Request
 from jinja2_fragments.fastapi import Jinja2Blocks
 
@@ -12,9 +14,18 @@ templates = Jinja2Blocks(settings.TEMPLATE_DIR)
 def home(request: Request):
     template = "home.html"
     block_name = None
-    context = {"request": request}
+    # TODO: add to db entity
+    featured_products = [
+        {"name": "Duracell AA", "src_path": "../static/images/battery-duracell-aa.jpg"},
+        {"name": "Linux AA", "src_path": "../static/images/battery-linux-aa.jpg"},
+        {"name": "GP CR2032", "src_path": "../static/images/battery-gp-cr2032.jpg"},
+    ]
+    context = {
+        "request": request,
+        "featured_product": random.choice(featured_products),
+    }
     if request.headers.get("HX-Request"):
-        block_name = "content"
+        block_name = "featured_product"
     return templates.TemplateResponse(template, context, block_name=block_name)
 
 
